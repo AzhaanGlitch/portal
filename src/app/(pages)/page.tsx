122e07ac6a5fc11b0b8288756a970354e2e76433
Aryan-81 <llaryanllrajll@gmail.com>
2025-09-12 19:24:12 +0530

    fix the auth problems

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,12 @@
+
+import HeroPage from "@/components/Hero"
+import MissionSection from "@/components/MissionSection";
+
+export default function Home() {
+  return (
+    <main className="font-sans">
+      <HeroPage/>
+      <MissionSection/>
+    </main>
+  );
+}
